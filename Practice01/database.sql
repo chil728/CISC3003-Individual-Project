d39e3e30389bd10d096dec34760a2290cb138d14
Tom <111277223+chil728@users.noreply.github.com>
2025-05-10 21:17:46 +0800

    Delete Practice01 directory

--- Practice01/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ooplogin;  -- Database For Practice01
-
-CREATE TABLE users (
-    users_uid INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    users_uid TINYTEXT NOT NULL,
-    users_pwd LONGTEXT NOT NULL,
-    users_email TINYTEXT NOT NULL
-);
